af27cb603423ccce963be1b2221949c0313ed974
Angélica <angelicasalaicesarriola@gmail.com>
2020-11-16 05:18:20 -0700

    primeras funciones en haskell

---
+++ funcional/introduccion/funciones.hs
@@ -0,0 +1,8 @@
+--como hacer una suma
+--public void suma (a,b)(return a+b;)
+--ghc  -o funciones ./funciones.hs
+suma a b = a + b
+resta a b = a - b
+sumar a b c d e = a + b + c + d + e
+division a b = a `div` b
+pregunta a = not a
