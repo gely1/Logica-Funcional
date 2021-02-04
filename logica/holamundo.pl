49effe1ca572e66617fcb08c26e59e64cedaad35
Angélica <angelicasalaicesarriola@gmail.com>
2021-02-04 00:05:15 -0700

    El como Hola mundo en prolog

---
+++ logica/holamundo.pl
@@ -0,0 +1,10 @@
+valioso(oro).
+valioso(cobre).
+es_libro(cienanios).
+
+progenitor(clara,jose).
+progenitor(tomas,jose).
+progenitor(tomas,isabel).
+progenitor(jose,ana).
+progenitor(jose,patricia).
+progenitor(patricia,jaime).
