a84d1171f08e3c8e32d0784b2f5d6a2da12b8fb9
Angélica <angelicasalaicesarriola@gmail.com>
2021-02-04 12:34:20 -0700

    Revert "Hola mundo prolog"
    This reverts commit 4d96bc60cc669d8e582e25c38e215ba87e39b940.

--- logica/holamundo.pl
+++ logica/holamundo.pl
@@ -8,14 +8,3 @@ progenitor(tomas,isabel).
 progenitor(jose,ana).
 progenitor(jose,patricia).
 progenitor(patricia,jaime).
-hombre(jose).
-hombre(tomas).
-hombre(jaime).
-mujer(clara).
-mujer(isabel).
-mujer(ana).
-mujer(patricia).
-
-%tia
-tia(X,Y):- progenitor(Z,X), progenitor(Z,A), progenitor(A,Y), mujer(X).
-abuela(X,Y):- progenitor(X,Z), progenitor(Z,Y), mujer(X).
